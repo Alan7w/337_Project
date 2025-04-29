c1fdf0df79797601dcbcb7d94a430a6cfc45d974
advaitk1 <166771433+advaitk1@users.noreply.github.com>
2025-04-29 13:13:31 -0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+function readProducts() {
+    const dataPath = path.join(__dirname, '../data/products.json');
+    try {
+        const data = fs.readFileSync(dataPath, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading products:', err);
+        return [];
+    }
+}
 
 router.get('/', (req, res) => {
-    const products = req.app.locals.db.readProducts();
+    const products = readProducts();
     res.json(products);
 });
 
 router.get('/:id', (req, res) => {
-    const product = req.app.locals.db.getProductById(req.params.id);
+    const products = readProducts();
+    const product = products.find(p => p.id === req.params.id);
     if (product) {
         res.json(product);
     } else {
